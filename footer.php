0a92ba93a25a19ac929effb2e8f18d5bd3715ee8
Rezuan Khamukov <Khamukov19@gmail.com>
2021-05-01 22:38:28 +0300

    Added freelance work page

--- footer.php
+++ footer.php
@@ -1,4 +1,36 @@
 <footer class="footer mt-5">
+    <!--<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+        </div>
+    </div>
+    </div>-->
+    <div class="modal-body">
+        <h5>Popover in a modal</h5>
+        <p>This <a href="#" role="button" class="btn btn-secondary popover-test" title="Popover title" data-content="Popover body content is set in this attribute.">button</a> triggers a popover on click.</p>
+        <hr>
+        <h5>Tooltips in a modal</h5>
+        <p><a href="#" class="tooltip-test" title="Tooltip">This link</a> and <a href="#" class="tooltip-test" title="Tooltip">that link</a> have tooltips on hover.</p>
+    </div>
+    <script>
+        $('#myModal').on('shown.bs.modal', function () {
+            $('#myInput').trigger('focus')
+        })
+    </script>
+
     <div class="container">
         <div class="d-flex justify-content-between">
             <span>© 2020 GoGameSchool.</span>
--- learning.php
+++ learning.php
@@ -19,11 +19,83 @@
     <main class="content-wrapper">
         <div class="container">
             <h1 class="text-center">Обучение</h1>
-            <div class="row mt-3">
+            <div class="input-group mb-3 pl-2 pr-2">
+                <input type="text" class="form-control" placeholder="Программирование на С#" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary bg-primary text-light" type="button">Найти</button>
+                </div>
+            </div>
+            <div class="grid-container">
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-primary">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">Программирование на С#</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
                 <a class="course-btn" href="unity/unitystart.php">
                     <button class="btn btn-secondary btn-case">
                         <span class="direction-progress">0%</span>
-                        <p class="direction-text">Разработка игр</p> 
+                        <p class="direction-text">Математика</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-primary">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">Физика</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-danger">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">Python</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-success">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">Unity</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-warning">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">Unreal Engine</p> 
+                        <div class="direction-param">
+                            <span>5 модулей</span>
+                            <span>24 часа</span>
+                            <span>1528 участников</span>
+                        </div>
+                    </button>
+                </a>
+                <a class="course-btn" href="unity/unitystart.php">
+                    <button class="btn btn-secondary btn-case bg-success">
+                        <span class="direction-progress">0%</span>
+                        <p class="direction-text">3D моделирование в Blender</p> 
                         <div class="direction-param">
                             <span>5 модулей</span>
                             <span>24 часа</span>
